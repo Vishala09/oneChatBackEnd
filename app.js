b358b642d835745f1ca1b8d36984617777e8d888
Vishala Ramasamy <vishala.ramasamy@gmail.com>
2020-11-21 02:32:17 +0530

    all messages

--- app.js
+++ app.js
@@ -4,23 +4,27 @@ const io = require("socket.io")(server, {
 });
   const users={};
   const usersarr=[];
+  const names=[];
   const messages=[];
 io.on('connection',(socket) => {
     //console.log("New Connection",socket.id);
     socket.on('chat',function(data){ 
         //console.log(data);
         //To show all the messages
-        //messages.push(data);io.sockets.emit('chatData',messages);
+        messages.push(data);io.sockets.emit('chatData',messages);
         //To show messages only if they are logged in
-         io.sockets.emit('chatData',data);
+        //io.sockets.emit('chatData',data);
     });
     
 
     socket.on('items',function(data){
-      io.sockets.emit('getItems',data)
+      messages.push(data);
+      io.sockets.emit('getItems',messages)
     });
+
+   // io.sockets.emit('usersnames',names);
     
-       io.sockets.emit('getUsers',usersarr);
+    io.sockets.emit('getUsers',usersarr);
 
     socket.on('name',function(data){
         const user = {
@@ -29,10 +33,11 @@ io.on('connection',(socket) => {
           };
           users[socket.id] = user;
           usersarr.push(user);
+          
         io.sockets.emit('myId',user);
-        //io.sockets.emit('users',Object.values(users));
+        io.sockets.emit('users',Object.values(users));
         io.sockets.emit('getUsers',usersarr);
-        console.log('newUser',data,users)
+        console.log('newUser',data,users);
     });
    
 })
